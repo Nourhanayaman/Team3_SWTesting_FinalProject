b426ffe040e1a82bb16fb4e6899e28bd6dda1fe6
AbdullahMahm0ud <Abdullah.BusinessME@gmail.com>
2025-05-04 07:52:50 +0300

    Update ProdSort.java

--- depiFinalProject/src/test/java/com/automation/SortingSys/ProdSort.java
+++ depiFinalProject/src/test/java/com/automation/SortingSys/ProdSort.java
@@ -1,103 +1,81 @@
 package com.automation.SortingSys;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.lang.Thread;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.automation.base.BaseTest;
 
-
-public class ProdSort {
-
-    WebDriver myBrowser;
+public class ProdSort extends BaseTest {
 
     private void selectSortOption(String value) {
-        myBrowser.findElement(By.className("product_sort_container")).click();
-        myBrowser.findElement(By.cssSelector("option[value=\"" + value + "\"]")).click();
+        driver.findElement(By.className("product_sort_container")).click();
+        driver.findElement(By.cssSelector("option[value=\"" + value + "\"]")).click();
     }
     
-
-    @BeforeEach
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        myBrowser = new ChromeDriver();
-        myBrowser.manage().window().maximize();
-
-        LoginHelper.performLogin(myBrowser);
-    }
-
     @Test
     public void testSorting() throws InterruptedException {
+        // Login
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        
         selectSortOption("az"); //First Sort Test
         Thread.sleep(1000);
 
-        List<String> ProductNames = new ArrayList<>();
-        List<WebElement> nameElements = myBrowser.findElements(By.className("inventory_item_name"));
+        List<String> productNames = new ArrayList<>();
+        List<WebElement> nameElements = driver.findElements(By.className("inventory_item_name"));
 
         for(WebElement element : nameElements) {
-            ProductNames.add(element.getText());
+            productNames.add(element.getText());
         }
 
-        List<String> ExpectedAZ = ExpectedList.SortedNamesAZ();
-        assertEquals(ExpectedAZ, ProductNames);
-        ProductNames.clear();
+        List<String> expectedAZ = ExpectedList.sortedNamesAZ();
+        assertEquals(productNames, expectedAZ);
+        productNames.clear();
 
         selectSortOption("za"); //Second Sort Test
         Thread.sleep(1000);
 
-        nameElements = myBrowser.findElements(By.className("inventory_item_name"));
+        nameElements = driver.findElements(By.className("inventory_item_name"));
 
         for(WebElement element : nameElements) {
-            ProductNames.add(element.getText());
+            productNames.add(element.getText());
         }
 
-        List<String> ExpectedZA = ExpectedList.SortedNamesZA();
-        assertEquals(ExpectedZA, ProductNames);
-        ProductNames.clear();
+        List<String> expectedZA = ExpectedList.sortedNamesZA();
+        assertEquals(productNames, expectedZA);
+        productNames.clear();
 
         selectSortOption("lohi"); //Third Sort Test
         Thread.sleep(1000);
 
-        List<String> ProductPrices = new ArrayList<>();
-        List<WebElement> PriceElements = myBrowser.findElements(By.className("inventory_item_price"));
+        List<String> productPrices = new ArrayList<>();
+        List<WebElement> priceElements = driver.findElements(By.className("inventory_item_price"));
 
-        for(WebElement element : PriceElements) {
-            ProductPrices.add(element.getText());
+        for(WebElement element : priceElements) {
+            productPrices.add(element.getText());
         }
 
-        List<String> ExpectedLOHI = ExpectedList.SortedPricesLH();
-        assertEquals(ExpectedLOHI, ProductPrices);
-        ProductPrices.clear();
+        List<String> expectedLOHI = ExpectedList.sortedPricesLH();
+        assertEquals(productPrices, expectedLOHI);
+        productPrices.clear();
 
         selectSortOption("hilo"); //Fourth Sort Test
         Thread.sleep(1000);
 
-        PriceElements = myBrowser.findElements(By.className("inventory_item_price"));
+        priceElements = driver.findElements(By.className("inventory_item_price"));
 
-        for(WebElement element : PriceElements) {
-            ProductPrices.add(element.getText());
+        for(WebElement element : priceElements) {
+            productPrices.add(element.getText());
         }
 
-        List<String> ExpectedHILO = ExpectedList.SortedPricesHL();
-        assertEquals(ExpectedHILO, ProductPrices);
-
-    }
-
-    @AfterEach
-    public void tearDown() {
-        if (myBrowser != null) {
-            myBrowser.quit();
-        }
+        List<String> expectedHILO = ExpectedList.sortedPricesHL();
+        assertEquals(productPrices, expectedHILO);
     }
 }
