30885d05d8c815f13d567b8faa531f5599014ced
AbdullahMahm0ud <Abdullah.BusinessME@gmail.com>
2025-05-04 07:53:49 +0300

    Update SortPerform.java

--- depiFinalProject/src/test/java/com/automation/SortingSys/SortPerform.java
+++ depiFinalProject/src/test/java/com/automation/SortingSys/SortPerform.java
@@ -1,76 +1,57 @@
 package com.automation.SortingSys;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 import java.util.Map;
 import java.util.Random;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.automation.base.BaseTest;
 
-public class SortPerform {
+public class SortPerform extends BaseTest {
     
-    WebDriver myBrowser;
-
     private void selectSortOption(String value) {
-        myBrowser.findElement(By.className("product_sort_container")).click();
-        myBrowser.findElement(By.cssSelector("option[value=\"" + value + "\"]")).click();
+        driver.findElement(By.className("product_sort_container")).click();
+        driver.findElement(By.cssSelector("option[value=\"" + value + "\"]")).click();
     }
 
     Map<String, String> expectedFirstItems = Map.of(
-    "az", "Sauce Labs Backpack",
-    "za", "Test.allTheThings() T-Shirt (Red)",
-    "lohi", "$7.99",
-    "hilo", "$49.99"
+        "az", "Sauce Labs Backpack",
+        "za", "Test.allTheThings() T-Shirt (Red)",
+        "lohi", "$7.99",
+        "hilo", "$49.99"
     );
 
     String[] sortOptions = {"az", "za", "lohi", "hilo"};
     Random rand = new Random();
 
-    @BeforeEach
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        myBrowser = new ChromeDriver();
-        myBrowser.manage().window().maximize();
-
-        LoginHelper.performLogin(myBrowser);
-    }
-
-
     @Test
     public void testSorting() throws InterruptedException {
+        // Login
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        
         for (int i = 0; i < 50; i++) {
-            String SelectedSort = sortOptions[rand.nextInt(sortOptions.length)];
-            selectSortOption(SelectedSort);
+            String selectedSort = sortOptions[rand.nextInt(sortOptions.length)];
+            selectSortOption(selectedSort);
 
             Thread.sleep(100);
 
-            String expectedFirst = expectedFirstItems.get(SelectedSort);
+            String expectedFirst = expectedFirstItems.get(selectedSort);
 
             WebElement firstItem;
-            if(SelectedSort.equals("az") || SelectedSort.equals("za")) {
-                firstItem = myBrowser.findElements(By.className("inventory_item_name")).get(0);
+            if(selectedSort.equals("az") || selectedSort.equals("za")) {
+                firstItem = driver.findElements(By.className("inventory_item_name")).get(0);
             } else {
-                firstItem = myBrowser.findElements(By.className("inventory_item_price")).get(0);
+                firstItem = driver.findElements(By.className("inventory_item_price")).get(0);
             }
 
-            System.out.println("Sort: " + SelectedSort + " → Found: " + firstItem.getText());
-            assertEquals(expectedFirst, firstItem.getText());
-        }
-    }
-
-
-    @AfterEach
-    public void tearDown() {
-        if (myBrowser != null) {
-            myBrowser.quit();
+            System.out.println("Sort: " + selectedSort + " → Found: " + firstItem.getText());
+            assertEquals(firstItem.getText(), expectedFirst);
         }
     }
 }
