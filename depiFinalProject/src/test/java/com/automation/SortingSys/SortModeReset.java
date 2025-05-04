36181642b1294d2fffd3db714f4fe0a983f9f737
AbdullahMahm0ud <Abdullah.BusinessME@gmail.com>
2025-05-04 07:53:36 +0300

    Update SortModeReset.java

--- depiFinalProject/src/test/java/com/automation/SortingSys/SortModeReset.java
+++ depiFinalProject/src/test/java/com/automation/SortingSys/SortModeReset.java
@@ -1,71 +1,53 @@
 package com.automation.SortingSys;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.assertEquals;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.automation.base.BaseTest;
 
-public class SortModeReset {
+public class SortModeReset extends BaseTest {
     
-
-    WebDriver Browser;
-
-    @BeforeEach
-    public void startUp() {
-        WebDriverManager.chromedriver().setup();
-        Browser = new ChromeDriver();
-        Browser.manage().window().maximize();
-
-        LoginHelper.performLogin(Browser);
-    }
-
     @Test
-    public void SortReset() throws InterruptedException {
-
-        Browser.findElement(By.className("product_sort_container")).click();
-        Browser.findElement(By.cssSelector("option[value='hilo']")).click(); //You can change to other opt ['lohi', 'za']
+    public void sortReset() throws InterruptedException {
+        // Login
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        
+        // Select high to low sort option
+        driver.findElement(By.className("product_sort_container")).click();
+        driver.findElement(By.cssSelector("option[value='hilo']")).click(); //You can change to other opt ['lohi', 'za']
         Thread.sleep(500);
 
-        String hiloOutput = Browser.findElement(By.className("inventory_item_name")).getText();
+        String hiloOutput = driver.findElement(By.className("inventory_item_name")).getText();
         String hiloExpected = "Sauce Labs Fleece Jacket";
         assertEquals(hiloOutput, hiloExpected);
         Thread.sleep(500);
 
-        Actions refresh = new Actions(Browser);
+        Actions refresh = new Actions(driver);
         refresh.keyDown(Keys.CONTROL).sendKeys("r").keyUp(Keys.CONTROL).perform(); //First refresh method
         Thread.sleep(1000);
 
-        WebElement sortDropdown = Browser.findElement(By.className("product_sort_container"));
+        WebElement sortDropdown = driver.findElement(By.className("product_sort_container"));
         String selectedSortText = sortDropdown.getAttribute("value");
         System.out.println("First refresh, current sort value: " + selectedSortText);
         try {
-            assertEquals("az", selectedSortText);
+            assertEquals(selectedSortText, "az");
         } catch (AssertionError err) {
             System.out.println("Assertion Method 1 failed " + err.getMessage()); //Check debug console
         }
 
-        Browser.navigate().refresh(); //Second refresh method
+        driver.navigate().refresh(); //Second refresh method
         Thread.sleep(2000);
 
-        sortDropdown = Browser.findElement(By.className("product_sort_container"));
+        sortDropdown = driver.findElement(By.className("product_sort_container"));
         selectedSortText = sortDropdown.getAttribute("value");
         System.out.println("Second refresh, current sort value: " + selectedSortText); //Check debug console
-        assertEquals("az", selectedSortText);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        if(Browser != null) {
-            Browser.quit();
-        }
+        assertEquals(selectedSortText, "az");
     }
 }
