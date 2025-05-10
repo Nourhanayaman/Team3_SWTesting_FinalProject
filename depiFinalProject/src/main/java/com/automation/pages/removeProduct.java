a12c4e0e6773c9cfa96fbdca442bd418ef63cd6b
Nourhanayaman <132375525+Nourhanayaman@users.noreply.github.com>
2025-05-10 23:06:43 +0300

    reallocate nourine files

---
+++ depiFinalProject/src/main/java/com/automation/pages/removeProduct.java
@@ -0,0 +1,20 @@
+package com.automation.pages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class removeProduct {
+    WebDriver driver;
+    By removeButton = By.xpath("//button[@class()='cart_button']");
+//constructor
+    public removeProduct(WebDriver driver) {
+        this.driver = driver;
+    }
+//actions
+    public void removeProduct() {
+        driver.findElement(removeButton).click();
+    }
+
+
+    }
